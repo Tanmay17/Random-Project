88860a13ee54ff1a15eaed0c5493695e64a775ba
Tanmay Srivastava <tanmay@getinfino.com>
2020-10-09 20:03:50 +0530

    feat: Pagination

--- src/vaccine/handler.js
+++ src/vaccine/handler.js
@@ -2,6 +2,15 @@ const Joi = require( 'joi' );
 const { Plugin: { DataReader } } = require( '../../lib' );
 const { VaccineService } = require( '../../lib/service' );
 
+let fileData;
+try {
+    ( async () => {
+        fileData = await DataReader.getData();
+    } )();
+} catch( err ) {
+    console.log( err );
+}
+
 const get_vaccine_info = async function get_vaccine_info ( req, res ) {
 
     const { search, filterOn, sortType, sortOn, page } = req.query;
@@ -24,14 +33,13 @@ const get_vaccine_info = async function get_vaccine_info ( req, res ) {
     //     if ( err ) return res.sendStatus( 400 );
     // } );
 
-    let data;
     try {
 
-        data = await DataReader.getData();
+        var data;
 
         if ( search && filterOn ){
 
-            data = await VaccineService.filterData( data, search, filterOn );
+            data = await VaccineService.filterData( fileData, search, filterOn );
 
             if ( !data ) {
 
@@ -43,7 +51,7 @@ const get_vaccine_info = async function get_vaccine_info ( req, res ) {
 
         if ( sortType && sortOn ){
 
-            data = await VaccineService.sortData( data, sortType, sortOn );
+            data = await VaccineService.sortData( fileData, sortType, sortOn );
 
             if ( !data ) {
 
@@ -53,7 +61,20 @@ const get_vaccine_info = async function get_vaccine_info ( req, res ) {
 
         }
 
-        return res.send( data ).status( 200 );
+        if ( !search && !filterOn ){
+
+            const paginatedData = await VaccineService.paginatedData( fileData, page );
+
+            if ( !paginatedData ) {
+
+                return res.sendStatus( 422 );
+
+            }
+
+            return res.send( { data: paginatedData } ).status( 200 );
+
+        }
+        return res.send( { data } ).status( 200 );
 
     } catch( error ) {
 
